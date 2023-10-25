e11a7aebcc94896fbb1528d3216ab82a36bed156
anderrf <anderson_rocha_farias@outlook.com>
2023-10-25 11:21:02 -0300

    feat: refresh token route

--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { authenticate } from './authenticate'
+import { refresh } from './refresh'
 
 export async function organizationsRoutes(app: FastifyInstance) {
   app.post('/organizations', create)
   app.post('/organizations/sessions', authenticate)
+  app.patch('/organizations/token/refresh', refresh)
 }
